bbdf297a61f8138126d12a353b1f38ed148c0006
unknown <zhivko.nikolov@infinno.eu>
2021-12-15 23:27:07 +0200

    theme set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      here
+      <Typography variant="h1" color="primary">Template with redux and persistor + MUI </Typography>
     </div>
   );
 }
